5d2d8d01ec687ab9af6113d7bf4993398f5b12cc
Raj Ranjan <raj.ranjan@alepo.com>
2016-04-19 16:59:13 +0530

    moved public files from ;ublic folder to root folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var sass=require('gulp-sass');
 
 gulp.task("serverTask",function(){
     connect.server({
-    	root : 'public',
     	livereload : true
     });
 });
@@ -15,12 +14,12 @@ gulp.task('browserifyTask',function(){
 	browserify("./app/app.js")
 	.bundle()
 	.pipe(source('main.js'))
-	.pipe(gulp.dest('./public/js/'));
+	.pipe(gulp.dest('./js/'));
 });
 gulp.task('cssTask',function(){
      gulp.src('./app/css/scss/**/*.scss')
      .pipe(sass().on('error',sass.logError))
-     .pipe(gulp.dest('./public/css'));
+     .pipe(gulp.dest('./css'));
 });
 gulp.task('watchTask',function(){
 	//when we are wathing all the js files any change in js file will lead to run the browserify task
