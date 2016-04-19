619ff16923a78c1b8ee368a62eacc24df5f3d8f4
Raj Ranjan <raj.ranjan@alepo.com>
2016-04-19 23:34:47 +0530

    added timeline feature

--- js/main.js
+++ js/main.js
@@ -28,9 +28,172 @@ require('./components');
 
 },{"./components":4,"angular":11,"angular-ui-bootstrap":8,"angular-ui-router":9}],2:[function(require,module,exports){
 'use strict';
-module.exports=function($scope){
-	$scope.text="raj ranjan";
+
+/*
+{
+        title: "",
+        date: "",
+        summary: "",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+ }
+*/
+module.exports = function($scope) {
+    $scope.projectsData = [{
+        title: "Employee of the month",
+        date: "August 2015",
+        summary: "Choosen as the employee of the month",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+
+        title: "Cross Platform Mobile App Development",
+        date: "August 2015",
+        summary: "A mobile recharging cross platform app built on Phone gap / Cordovaand Ionic framework and AngularJs. Main target is for Android and ios.",
+        technology: ['Ionic', 'AngularJS', 'NodeJs'],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+
+        title: "JIRA Based Project and Employee Management tool",
+        date: "May 2015",
+        summary: "Rest API based tool for company’s project and employee management.",
+        technology: ['REST Integration', 'PHP'],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Appraisal Portal for Company",
+        date: "May 2015",
+        summary: "A 360 degree appraisal system designed for the company.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "College Alumni Portal | Social Networking Website",
+        date: "May 2015",
+        summary: "Alumni portal where people can be in touch with their college mates.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "ShareResume",
+        date: "June 2015",
+        summary: "Online Resume creation and sharing platform for people so that their resume and documents can be digitally stored.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check",
+            links: {
+                git: "https://github.com/RajRanjan/shareResume"
+            }
+        }
+    }, {
+        title: "The Asylum | Publishing House | Friend’s start-up",
+        date: "2013",
+        summary: "A friend’s start-up company as a publishing house. CSS3 heading feature has been included. The development took only 3 Hours due to urgent requirement for his book launch.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Library Management System| College Competition | 1 st Prize",
+        date: "2012",
+        summary: "Developed a library management system website in college competition. People can search books available and can issue them.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "College Fest Website",
+        date: "2012 – 2013",
+        summary: "Tecnoesis a techno management college fest of NIT Silchar. Worked as a team leader and guided juniors in best possible way.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Rahul Saini Personal Website | Author Website",
+        date: "2012",
+        summary: "A personal website of an author with his books review. A blog system for the author. Email contactsystem was integrated.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Manila Expat Services Network | Internship",
+        date: "2012",
+        summary: "Worked as a PHP developer for conndignsolutions to develop the server side of service providing website.Worked with foreign clients.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Fountainpenguild | Literary Social Network",
+        date: "2011",
+        summary: "A junior put up a start-up where writers can talk share and read articles.It gave a platform to young writers to showcase their talents. Object-Oriented PHP was used.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Hostel website on college LAN",
+        date: "2010",
+        summary: "A discussion forum for all the branches of college along with a college news portal was developed. First ever experience with wampserver and database and PHP language.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Chemistry Online tutor ial | Yahoo Geocities |Class 9",
+        date: "2006",
+        summary: "First experience of dealing with server and publish a website online. Idea was to help the passing out batch mates with a comprehensive chemistry tutorial website.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Quad copter | Research Promotion Cell NIT Silchar",
+        date: "2014",
+        summary: "A 6 degree freedom quad copter project is under progress. This project involves use of Arduino, Inertial Measurement Unit (gyroscope, accelerometer, and magnetometer), and 6 x wireless unit.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "PC Controlled Robot | UART Serial Communication",
+        date: "2013",
+        summary: "Robot can be controlled by the keyboard keys W, S, A, D. UART communication of AVR microcontroller was used. An overview of RS-232 protocol and TTL logics were learnt",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Cell phone Operated Robot",
+        date: "2012",
+        summary: "A cell phone dials a number of a mobile which is attached to the robot and the robot can be controlled by pressing keypad on the cell phone Worked on the DTMF Signals and DTMF decoder MT8870.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }, {
+        title: "Line Follower Robot",
+        date: "2010",
+        summary: "An infrared operated robot which can sense black and white surface and take appropriate decision by doing calculations in AVR microcontroller. Learned about Architecture of At mega 32, Light sensors, Voltage Regulators and other Integrated circuits.",
+        technology: [],
+        resource: {
+            icon: "glyphicon glyphicon-check"
+        }
+    }];
+
 };
+
 },{}],3:[function(require,module,exports){
 var app=require('angular').module('rajRanjan')
 
@@ -43,8 +206,11 @@ var app=require('angular').module('rajRanjan')
 
 app.controller('projectController',require('./projectController'));
 },{"./projectController":6,"angular":11}],6:[function(require,module,exports){
-arguments[4][2][0].apply(exports,arguments)
-},{"dup":2}],7:[function(require,module,exports){
+'use strict';
+module.exports=function($scope){
+	$scope.text="raj ranjan";
+};
+},{}],7:[function(require,module,exports){
 /*
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
